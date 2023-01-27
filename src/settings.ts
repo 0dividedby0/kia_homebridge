ceb10dad723b01809b15eb72eeb002ff903a09cb
0dividedby0 <jasonth14@gmail.com>
2023-01-26 23:14:00 -0800

    Began homebridge integration

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'KiaConnectHomebridgePlugin';
+export const PLATFORM_NAME = 'Homebridge Kia Connect';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = '@0dividedby0/kia-connect-homebridge-plugin';
+export const PLUGIN_NAME = 'homebridge-kia-connect';
